63896bde54578eea19954b639ad19f7357da1a12
Pretzels14 <connorp54@gmail.com>
2024-02-14 01:22:31 -0600

    Add project files.

---
+++ PokemonApp/PokemonPlayer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonApp
+{
+    internal class PokemonPlayer
+    {
+        // declare our variables of the user, such as the name, badgees, and their pokemon caught and battles won
+        public string TrainerName { get; set; }
+        public int TrainerBadges { get; set; }
+        private int caughtPokemon;
+        private int battlesWon;
+
+        // Use property to acccess the caughtPokemon variable
+        public int CaughtPokemon
+        {
+            get { return caughtPokemon; } // return the caughtPokemon
+            
+            // Set the caughtPokemon to be equal to the value entered, or 0 if there are no caught pokemon
+            set
+            {
+                if (value >= 0)
+                {
+                    caughtPokemon = value;
+                }
+                else
+                    caughtPokemon = 0;
+            }
+        }
+
+        // Use property to acccess thee battlesWon variable
+
+        public int BattlesWon
+        {
+            get { return battlesWon; } // returns the number of battles won
+           
+            // Set's the battles won to be equal to the value inputted, and defaults to zero if there is no value
+            set
+            {
+                if (value >= 0) 
+                    battlesWon = value;
+                else 
+                    battlesWon = 0;
+            }
+        }
+
+        // Set default constructor for our values, using an empty string as the default name and 0 for the ints
+        public PokemonPlayer()
+        {
+            TrainerName = "Unknown";
+            TrainerBadges = 0;
+            caughtPokemon = 0;
+            battlesWon = 0;
+        }
+        // Set our actual constructor, which will take in a name and number of badges, pokemon caught, and battles won
+        public PokemonPlayer(string trainerName, int trainerBadges, int caughtPokemon, int battlesWon)
+        {
+            TrainerName = trainerName;
+            TrainerBadges = trainerBadges;
+            this.caughtPokemon = caughtPokemon;
+            this.battlesWon = battlesWon;
+        }
+
+        // This method will be used to calculate the player's win rate in battle, using their battles won and pokemon caught
+        public double trainerWinRate()
+        {
+            // if no battles have been won, the win rate will be zero, elsee return the value of their battles won divided by the total of their won battles and caught pokemon
+            if (battlesWon == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (double)battlesWon / (battlesWon + caughtPokemon);
+            }
+        }
+
+        // ToString method that allows our user to see properly displayed information of what they entered and the calculated information
+        public override string ToString()
+        {
+            return $"Trainer Name: {TrainerName}\nTrainerBadges: {TrainerBadges}\nNumber of Pokemon Caught: {CaughtPokemon}\nNumber of Trainer Battles Won: {BattlesWon}";
+        }
+    }
+}
+
---
+++ PokemonApp/Program.cs
@@ -0,0 +1,92 @@
+﻿
+/**
+ * This program will display basic information based about a player who is playing Pokemon,
+ * such as the trainer name, badges, and then will dislplay a 'win rate' of their battles 
+ * using their battles they've won, and how many pokemon they've caught
+ */
+
+
+using static System.Console;
+
+namespace PokemonApp
+{
+    internal class Program
+    {
+        // This method will be called in main to print a heading for the user
+        static void PrintHeading()
+        {
+            WriteLine("***************************************************************");
+            WriteLine("                     Pokemon Trainer Info App");
+            WriteLine("***************************************************************");
+            WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            PrintHeading();
+
+            // variable to determine whether or not our user will keep entering information
+            bool keepGoing = true;
+
+            // variables that will hold the trainers name, their badges, pokemon caught and battles won, as well as the win rate and an input string
+            string trainerName;
+            int numberOfBadges;
+            int caughtPokemon;
+            int trainerBattlesWon;
+            double trainerWinRate;
+            string input;
+
+
+            do
+            {
+                try
+                {
+                    // Prompt user to enter the information such as their name, number of badges, number of pokemon caught, and how many battles they've won
+
+                    WriteLine("Enter the information about your trainer:");
+
+                    Write("Trainer's Name: ");
+                    trainerName = Console.ReadLine();
+
+                    Write("Number of Badges: ");
+                    numberOfBadges = int.Parse(Console.ReadLine());
+
+                    Write("Number of Pokemon Caught: ");
+                    caughtPokemon = int.Parse(Console.ReadLine());
+
+                    Write("Number of trainer battles won: ");
+                    trainerBattlesWon = int.Parse(Console.ReadLine());
+                    WriteLine();
+
+                    // Now that we have the information, create a object from the PokemonPlayer class
+                    PokemonPlayer player = new PokemonPlayer(trainerName, numberOfBadges, caughtPokemon, trainerBattlesWon);
+
+                    // Display the information about the trainer to the user using the ToString
+                    WriteLine("Here's your trainer's information: ");
+                    WriteLine(player.ToString());
+
+                    // Calculate the player's win rate in battle and display it to them
+                    trainerWinRate = player.trainerWinRate();
+                    WriteLine($"Trainer Win Rate: {trainerWinRate:P}");
+
+                    // Ask the user if they would like to enter more information, and if so keep the program running or else quit the program
+                    Write("\nDo you want to enter more trainers? (y/n): ");
+                    input = ReadLine();
+                    if (input.ToLower() == "n")
+                    {
+                        keepGoing = false;
+                    } else
+                    {
+                        keepGoing = true;
+                    }
+
+                } catch (Exception e)
+                {
+                    WriteLine("\nYou made an error in your input ");
+                    WriteLine($"Here is the error message: {e.Message} \nResetting the program.");
+                    keepGoing= true;
+                }
+            } while (keepGoing);
+            WriteLine("\nThank you for using the Pokemon Trainer App");
+        }
+    }
+}
